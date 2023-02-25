2a1f837090979fa7de97f873408cba00c516737e
dmitrykalmanovich@gmail.com <dmitrykalmanovich@gmail.com>
2023-02-25 18:48:41 +0000

    mc resolved

--- macros/get_payment_type_description.sql
+++ macros/get_payment_type_description.sql
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
  {#
     This macro returns the description of the payment_type 
 #}
@@ -15,24 +15,3 @@
     end
 
 {%- endmacro %}
-
-=======
- {#
-    This macro returns the description of the payment_type 
-#}
-
-{% macro get_payment_type_description(payment_type) -%}
-
-    case {{ payment_type }}
-        when 1 then 'Credit card'
-        when 2 then 'Cash'
-        when 3 then 'No charge'
-        when 4 then 'Dispute'
-        when 5 then 'Unknown'
-        when 6 then 'Voided trip'
-    end
-
-{%- endmacro %}
-
->>>>>>> c820244090225e40cf595eaedaa37638bd33d281
-              
